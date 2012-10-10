1a8f0f5228715418fd18aa194de344e35a957767
David Barker <djb218@cam.ac.uk>
2012-10-10 15:33:02 +0100

    Basic project structure set up for initial tests and demos

--- ArrowDataBinding/Program.cs
+++ ArrowDataBinding/Program.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ArrowDataBinding.tests;
 
 namespace ArrowDataBinding
 {
-    class Tester
+    class Program
     {
         static void Main(string[] args)
         {
