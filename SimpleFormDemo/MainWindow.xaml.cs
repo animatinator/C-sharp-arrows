46e19714b3cf17a89fa2fae7d944ca659d35e4bb
David Barker <djb218@cam.ac.uk>
2012-12-23 22:09:16 +0000

    Did a bir of work on the project for simple binding demos

--- SimpleFormDemo/MainWindow.xaml.cs
+++ SimpleFormDemo/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ArrowDataBinding.Arrows;
+using ArrowDataBinding.Combinators;
 
 namespace SimpleFormDemo
 {
@@ -20,9 +22,29 @@ namespace SimpleFormDemo
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Arrow<int, Tuple<int, int>> circle = null;
+        private int x = 10;
+        private int y = 10;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitialiseArrows();
+        }
+
+        public void InitialiseArrows()
+        {
+            InitialiseCircleArrow();
+        }
+
+        public void InitialiseCircleArrow()
+        {
+            Arrow<int, Tuple<int, int>> timeDup = Op.Split<int>();
+            Arrow<int, double> sin = Op.Arr((int x) => Math.Sin((double)x / 10));
+            Arrow<int, double> cos = Op.Arr((int y) => Math.Cos((double)y / 10));
+            Arrow<double, int> doubleToInt = Op.Arr((double x) => (int)x);
+            Arrow<Tuple<double, double>, Tuple<int, int>> adapter = doubleToInt.And(doubleToInt);
+            Arrow<int, Tuple<int, int>> circle = timeDup.Combine(sin.And(cos)).Combine(adapter);
         }
     }
 }
